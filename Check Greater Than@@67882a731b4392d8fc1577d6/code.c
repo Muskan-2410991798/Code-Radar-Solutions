89245497db5c2ed3917a4df10d9010258396a26d
Muskan-2410991798 <muskan1798.becse24@chitkara.edu.in>
2025-01-29 23:56:17 +0530

    Solution to Check Greater Than

---
+++ Check Greater Than@@67882a731b4392d8fc1577d6/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main() {
+    int a,b;
+    scanf("%d %d",&sa,&b);
+    if(a>b){
+        printf("True");
+    }
+    else{
+        printf("False");
+    }
+    return 0;
+}
