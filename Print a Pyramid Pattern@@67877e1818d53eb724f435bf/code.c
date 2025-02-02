dc1946902c5ec2dd9ba6cd4f968629dc430c50ca
Muskan-2410991798 <muskan1798.becse24@chitkara.edu.in>
2025-02-02 12:21:08 +0530

    Solution to Print a Pyramid Pattern

---
+++ Print a Pyramid Pattern@@67877e1818d53eb724f435bf/code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            printf(" ");
+        }
+        printf("\n");
+        for(int j=0;j<2n+1;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
