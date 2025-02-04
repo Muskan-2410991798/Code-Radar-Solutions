91862d35478d07f13d658faf0d36fa3b2cd73de7
Muskan-2410991798 <muskan1798.becse24@chitkara.edu.in>
2025-02-04 11:12:20 +0530

    Solution to Find the Index of a Target Element in an Array

--- Find the Index of a Target Element in an Array@@676c02002dbc8c3e7a280b77/code.c
+++ Find the Index of a Target Element in an Array@@676c02002dbc8c3e7a280b77/code.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-int ans(,int n,int arr[n],int target){
+int ans(int n,int arr[n],int target){
     for(int i=0;i<n;i++){
         if(arr[i]==target){
-            printf("%d",i);
-            break;
+            return i;
         }
     }
     return -1;
